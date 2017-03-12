5b9665ef1b32f225538ab94131bef6540ce2fdf3
Stanislav Perchenko <sperchenko@ssa-outsourcing.com>
2017-03-12 21:41:20 +0200

    Framework of the new devive verification controller

--- app/src/main/java/com/alperez/bt_microphone/GlobalConstants.java
+++ app/src/main/java/com/alperez/bt_microphone/GlobalConstants.java
@@ -11,10 +11,10 @@ public class GlobalConstants {
     public static final int DB_VERSION = 3;
 
 
-    public static final UUID UUID_SERVICE_1 = UUID.fromString("6705ef4c-eb38-49a2-9a7b-679e52aeb3d6");
-    public static final UUID UUID_SERVICE_2 = UUID.fromString("6705ef4c-eb38-49a2-9a7b-679e065ab3d6");
+    public static final UUID UUID_SERVICE_1 = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+    //public static final UUID UUID_SERVICE_2 = UUID.fromString("6705ef4c-eb38-49a2-9a7b-679e065ab3d6");
 
-    public static final UUID[] ALL_SERVICE_UUIDS = {UUID_SERVICE_1, UUID_SERVICE_2};
+    public static final UUID[] ALL_SERVICE_UUIDS = {UUID_SERVICE_1/*, UUID_SERVICE_2*/};
 
     public static final int MAX_TIME_FOR_SDP = 15000;
     public static final int MAX_TIME_AFTER_LAST_UUID_DISCOVERED = 6000;
--- app/src/main/java/com/alperez/bt_microphone/bluetoorh/management/DiscoveredDeviceVerifier.java
+++ app/src/main/java/com/alperez/bt_microphone/bluetoorh/management/DiscoveredDeviceVerifier.java
@@ -1,5 +1,9 @@
 package com.alperez.bt_microphone.bluetoorh.management;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+
 import com.alperez.bt_microphone.model.DiscoveredBluetoothDevice;
 import com.alperez.bt_microphone.model.ValidBtDevice;
 import com.alperez.bt_microphone.utils.Callback;
@@ -13,6 +17,11 @@ public class DiscoveredDeviceVerifier {
     private Callback<ValidBtDevice> resultCallback;
 
 
+    private volatile boolean released;
+    private final Thread worker;
+    private final Handler resultHandler;
+
+
     public static DiscoveredDeviceVerifier createForDevice(DiscoveredBluetoothDevice device) {
         return new DiscoveredDeviceVerifier(device);
     }
@@ -20,7 +29,34 @@ public class DiscoveredDeviceVerifier {
 
     private DiscoveredDeviceVerifier(DiscoveredBluetoothDevice device) {
         this.device = device;
-        //TODO Implement this!!!!!!!!!!!!!!!!!!!!!
+
+        resultHandler = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+                if (resultCallback != null) {
+                    if (msg.obj instanceof ValidBtDevice) {
+                        resultCallback.onComplete((ValidBtDevice) msg.obj);
+                    } else if (msg.obj instanceof Throwable) {
+                        resultCallback.onError((Throwable) msg.obj);
+                    }
+                }
+            }
+        };
+
+        worker = new Thread(getClass().getSimpleName()) {
+            @Override
+            public void run() {
+                try {
+                    ValidBtDevice result = doTheJobBackground();
+                    resultHandler.obtainMessage(0, result).sendToTarget();
+                } catch (InterruptedException e) {
+                    // Do nothing in this case
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    resultHandler.obtainMessage(0, e).sendToTarget();
+                }
+            }
+        };
     }
 
     public DiscoveredDeviceVerifier withResultCallback(Callback<ValidBtDevice> callback) {
@@ -30,11 +66,35 @@ public class DiscoveredDeviceVerifier {
 
 
     public DiscoveredDeviceVerifier start() {
-        //TODO Implement this!!!!!!!!!!!!!!!!!!!!!
+        worker.start();
         return this;
     }
 
     public void release() {
-//TODO Implement this!!!!!!!!!!!!!!!!!!!!!
+        released = true;
+        worker.interrupt();
+    }
+
+
+
+    private ValidBtDevice doTheJobBackground() throws Exception {
+
+        //--- Initial delay ---
+        Thread.sleep(600);
+
+
+        //---  Emulatye check ---
+        int n = 0;
+        while (!released && n++ < 150) {
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e){}
+        }
+
+        if (released) {
+            throw new InterruptedException("Worker released early!");
+        } else {
+            throw new Exception("Error device verification");
+        }
     }
 }
