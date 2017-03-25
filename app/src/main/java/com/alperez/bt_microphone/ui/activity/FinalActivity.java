7620d90e3bda812d615813418f8e9339fc8be650
Stanislav Perchenko <sperchenko@ssa-outsourcing.com>
2017-03-25 16:13:40 +0200

    tmop

---
+++ app/src/main/java/com/alperez/bt_microphone/ui/activity/FinalActivity.java
@@ -0,0 +1,10 @@
+package com.alperez.bt_microphone.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by stanislav.perchenko on 3/24/2017.
+ */
+
+public class FinalActivity extends AppCompatActivity {
+}
